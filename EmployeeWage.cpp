16c6af95dd1008dd2b1a0a71e733bc04f6ca29aa
“sandip5” <singhsandeep.singh97@gmail.com>
2020-08-26 06:28:24 +0530

    ADDED- Is Present Method Check Employee Present Or Absent

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
+bool isPresent()
+{
+    cout << "Hello Welcome To Employee Wage Computation Problem" << endl;
+	srand( time(0) );
+	int checkAttendance = rand() % 2 + 1;
+	
+    if( checkAttendance == 1 )
+	{
+        cout << "Employee Is Present";
+		return true;
+	}
+	else if( checkAttendance == 2 )
+	{
+		cout << "Employee Is Absent" << endl;
+        return false;
+	}
+    
+    return false;
+}
+
 int main()
 {
-    cout << "Hello Welcome To Employee Wage Computation Problims....\nHere We Follow OOPS Approch." << endl;
+    isPresent();
+    return 0;
 }
