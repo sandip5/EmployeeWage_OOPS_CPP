4441c03d7ed725a42986dff4c7df28901c6c3d2f
“sandip5” <singhsandeep.singh97@gmail.com>
2020-08-27 05:09:44 +0530

    ADDED- Ability To Add Total Wage For Each Company

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
 #include <ctime>
 #include <unistd.h>
+#include <vector>
 
 using namespace std;
 
 class EmpWageBuilder
 {
-    private:
-        const int FULL_TIME = 8;
-        const int PART_TIME = 4;
-        int maxWorkingDayInMonth = 20;
-        int maxHoursPerMonth = 100;
-        int wagePerHr = 20;
-	    int dailyWage = 0;
-        int monthWage = 0;
-        int empHrs = 0;
-        int totalEmpHrs = 0;
-        int totalWorkingDays = 0;
-        string companyName;
+    string companyName;
+    int totalWage;
+    int maxWorkingDayInMonth;
+    int maxHoursPerMonth;
+    int wagePerHr;
 
     public:
         EmpWageBuilder(string companyName, int wagePerHr, int maxWorkingDayInMonth, int maxHoursPerMonth);
+        void setDetails(int totalWage);
+        string getCompanyName();
+        int getTotalWage();
+        int getWagePerHr();
+        int getMaxWorkingDayInMonth();
+        int getMaxHoursPerMonth();
+};
+
+class CompanyEmpWage
+{
+    private:
+        vector<EmpWageBuilder> companies;
+
+    public:
         int getEmployeeWorkingHours();
-        int calculateDailyWage();
-        void calculateMonthWage();
+        int calculateDailyWage(int wagePerHr);
+        void calculateMonthWage(EmpWageBuilder details);
+        void saveDetails(EmpWageBuilder details);
+        void showSavedWage();
 };
 
 EmpWageBuilder::EmpWageBuilder(string companyName, int wagePerHr, int maxWorkingDayInMonth, int maxHoursPerMonth)
@@ -34,10 +44,42 @@ EmpWageBuilder::EmpWageBuilder(string companyName, int wagePerHr, int maxWorking
     this -> maxHoursPerMonth = maxHoursPerMonth;
 }
 
-int EmpWageBuilder::getEmployeeWorkingHours()
+int EmpWageBuilder::getWagePerHr()
+{
+    return wagePerHr;
+}
+
+int EmpWageBuilder::getMaxWorkingDayInMonth()
+{
+    return maxWorkingDayInMonth;
+}
+
+int EmpWageBuilder::getMaxHoursPerMonth()
+{
+    return maxHoursPerMonth;
+}
+
+void EmpWageBuilder::setDetails(int totalWage)
+{
+    this -> totalWage = totalWage;
+}
+
+string EmpWageBuilder::getCompanyName()
+{
+    return companyName;
+}
+
+int EmpWageBuilder::getTotalWage()
+{
+    return totalWage;
+}
+
+int CompanyEmpWage::getEmployeeWorkingHours()
 {
     const int IS_FULL_TIME = 1;
 	const int IS_PART_TIME = 2;
+    const int FULL_TIME = 8;
+    const int PART_TIME = 4;
     const int ABSENT = 3;
     int empWorkDuration = 0;
 	
@@ -61,36 +103,56 @@ int EmpWageBuilder::getEmployeeWorkingHours()
     return empWorkDuration;
 }
 
-int EmpWageBuilder::calculateDailyWage()
+int CompanyEmpWage::calculateDailyWage(int wagePerHr)
 {
+    int dailyWage = 0;
     dailyWage = wagePerHr * getEmployeeWorkingHours();
 
 	cout << "Employee Daily Wage: " << dailyWage << endl;
     return dailyWage;
 }
 
-void EmpWageBuilder::calculateMonthWage()
+void CompanyEmpWage::calculateMonthWage(EmpWageBuilder details)
 {
-    while( totalEmpHrs < maxHoursPerMonth && totalWorkingDays < maxWorkingDayInMonth )
+    
+    int totalEmpHrs = 0;
+    int totalWorkingDays = 0;
+    int dailyWage = 0;
+    int monthWage = 0;
+
+    while( totalEmpHrs < details.getMaxHoursPerMonth() && totalWorkingDays < details.getMaxWorkingDayInMonth() )
     {
-        dailyWage = calculateDailyWage();
+        dailyWage = calculateDailyWage(details.getWagePerHr());
         monthWage = monthWage + dailyWage;
-        totalEmpHrs = totalEmpHrs + empHrs;
         totalWorkingDays++;
         sleep(1);
     }
 
-    cout << companyName << ", Monthly Wage Of Employee: " << monthWage << endl;
+    details.setDetails(monthWage);
+    cout << details.getCompanyName() << ", Monthly Wage Of Employee: " << monthWage << endl;
+    saveDetails(details);
+}
+
+void CompanyEmpWage::saveDetails(EmpWageBuilder details)
+{
+    companies.push_back(details);
+}
+
+void CompanyEmpWage::showSavedWage()
+{
+    for(EmpWageBuilder it : companies)
+	{
+		cout << it.getCompanyName() << ", Total Wage : " << it.getTotalWage() << endl; 
+	}
 }
 
 int main()
 {
     cout << "Hello Welcome To Employee Wage Computation Problem" << endl;
-    EmpWageBuilder *emp;
-    emp = new EmpWageBuilder("Apple", 200, 22, 180);
-    emp -> calculateMonthWage();
-    emp = new EmpWageBuilder("Microsoft", 150, 24, 200);
-    emp -> calculateMonthWage();
-    delete emp;
+    EmpWageBuilder apple("Apple", 200, 22, 180), microsoft("Microsoft", 150, 24, 200);
+    CompanyEmpWage companyEmpWage;
+    companyEmpWage.calculateMonthWage(apple);
+    companyEmpWage.calculateMonthWage(microsoft);
+    companyEmpWage.showSavedWage();
     return 0;
 }
