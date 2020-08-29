330446467d4de28f5a1dee30e116aec32adaac93
“sandip5” <singhsandeep.singh97@gmail.com>
2020-08-29 06:24:51 +0530

    REFACTORED- Present Method And Implementation User Can Not Access Internal Details

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -4,58 +4,122 @@
 
 using namespace company_emp_wage;
 
-CompanyEmpWage takeUserInput()
+struct CompanyInitialInfo
 {
-    cout << "Enter Number Of Company You Want To Add: " << endl;
-    int numberOfCompany;
-    cin >> numberOfCompany;
-    
-    emp_model_spc::EmpWageBuilder *emp[numberOfCompany];
-    
-    for(int iterator_company = 0; iterator_company < numberOfCompany; iterator_company++)
-    {
-        if (iterator_company > 0)
-        {
-            cout << "Enter Next New Company Details" << endl;
-        }
-        
-        cout << "Enter Company Name: " << endl;
-        string companyName;
-        cin >> companyName;
-        cout << "Enter Wage Per Hour: " << endl;
-        int wagePerHr;
-        cin >> wagePerHr;
-        cout << "Enter Number Of Working Days In A Month: " << endl;
-        int numberOfWorkingDaysInMonth;
-        cin >> numberOfWorkingDaysInMonth;
-        cout << "Enter Maximum Hours For A Month: " << endl;
-        int maxHoursInMonth;
-        cin >> maxHoursInMonth;
-        
-        emp[iterator_company] = new emp_model_spc::EmpWageBuilder(companyName, wagePerHr, numberOfWorkingDaysInMonth, maxHoursInMonth);
-    }
+    string companyName;
+    int wagePerHr;
+    int numberOfWorkingDaysInMonth;
+    int maxHoursInMonth;
+};
 
-    CompanyEmpWage companyEmpWage;
-    
-    for(int calculateForEach = 0; calculateForEach < numberOfCompany; calculateForEach++)
-        companyEmpWage.calculateMonthWage(*emp[calculateForEach]);
-
-    return companyEmpWage;
-}
-
-void presentEmpWageComputation()
-{
-    cout << "Hello Welcome To Employee Wage Computation Problem" << endl;
-    
-    CompanyEmpWage companyEmpWage = takeUserInput();
-    
-    emp_std::displayCompanyWages(companyEmpWage.getSavedDetails());
-    
-    emp_std::searchTotalWage(companyEmpWage.getSavedDetails());
-}
+void presentEmpWageComputation();
+CompanyInitialInfo takeUserInput();
 
 int main()
 {
     presentEmpWageComputation();
     return 0;
+}
+
+void presentEmpWageComputation()
+{
+    cout << "Hello Welcome To Employee Wage Computation Problem" << endl;
+
+    CompanyEmpWage companyEmpWage;
+
+    enum optionCategory
+    {
+        REGISTER_COMPANY,
+        CALCULATE_WAGE,
+        DISPLAY,
+        SEARCH_TOTAL_WAGE,
+        EXIT
+    };
+
+    bool isStart = false;
+
+    while (!isStart)
+    {
+        cout << "Select Operation Which You Want To Perform:\n1. Register Company Details..."
+             << "\n2. Calculate Wage For Company By Company Name...\n3. Display Wages Of All Company..."
+             << "\n4. Search Total Wage By Company Name...\n5. Exit" << endl;
+        int selectedOption;
+        cin >> selectedOption;
+
+        switch (selectedOption - 1)
+        {
+            case optionCategory::REGISTER_COMPANY:
+            {
+                cout << "Enter Number Of Company You Want To Add: " << endl;
+                int numberOfCompany;
+                cin >> numberOfCompany;
+                
+                CompanyInitialInfo initialInfo[numberOfCompany];
+                
+                for(int iterator_company = 0; iterator_company < numberOfCompany; iterator_company++)
+                {
+                    if (iterator_company > 0)
+                    {
+                        cout << "Enter Next New Company Details" << endl;
+                    }
+
+                    initialInfo[iterator_company] = takeUserInput();
+                    companyEmpWage.saveDetails(emp_model_spc::EmpWageBuilder
+                        (initialInfo[iterator_company].companyName, 
+                        initialInfo[iterator_company].wagePerHr, 
+                        initialInfo[iterator_company].numberOfWorkingDaysInMonth, 
+                        initialInfo[iterator_company].maxHoursInMonth));
+                }    
+                break;
+            }
+            case optionCategory::CALCULATE_WAGE:
+            {
+                string calculateByName;
+                cout << "Enter Valid Company Name For Which You Want to Calulate Wage: " << endl;
+                cin >> calculateByName;
+                companyEmpWage.calculateMonthWage(calculateByName);
+                break;
+            }
+            case optionCategory::DISPLAY:
+            {
+                emp_std::displayCompanyWages(companyEmpWage);
+                break;
+            }
+            case optionCategory::SEARCH_TOTAL_WAGE:
+            {
+                cout << "Enter Company Name: " << endl;
+                string companyName;
+                cin >> companyName;
+                emp_std::searchTotalWage(companyEmpWage, companyName);
+                break;
+            }
+            case optionCategory::EXIT:
+            {
+                isStart = true;
+                break;
+            }
+            default:
+                cout << "Please Enter Valid Input..." << endl; 
+                break;
+        }
+    }
+}
+
+CompanyInitialInfo takeUserInput()
+{
+    CompanyInitialInfo companyInitialInfo;
+
+    cout << "Enter Company Name: " << endl;
+    cin >> companyInitialInfo.companyName;
+
+    cout << "Enter Wage Per Hour: " << endl;
+    cin >> companyInitialInfo.wagePerHr;
+
+    cout << "Enter Number Of Working Days In A Month: " << endl;
+    cin >> companyInitialInfo.numberOfWorkingDaysInMonth;
+
+    cout << "Enter Maximum Hours For A Month: " << endl;
+    cin >> companyInitialInfo.maxHoursInMonth;
+
+    return companyInitialInfo;
 }
--- emp_utility.h
+++ emp_utility.h
@@ -4,23 +4,18 @@ using namespace std;
 
 namespace emp_std
 {
-    void displayCompanyWages(vector<emp_model_spc::EmpWageBuilder> companies)
+    void displayCompanyWages(company_emp_wage::CompanyEmpWage companyEmpWage)
     {
-        for(emp_model_spc::EmpWageBuilder it : companies)
+        for(emp_model_spc::EmpWageBuilder it : companyEmpWage.getSavedDetails())
 	    {
 		    cout << it.getCompanyName() << ", Total Wage : " << it.getTotalWage() << endl; 
 	    }
     }
 
-    void searchTotalWage(vector<emp_model_spc::EmpWageBuilder> companies)
+    void searchTotalWage(company_emp_wage::CompanyEmpWage companyEmpWage, string companyName)
     {
         int companyTotalWage = 0;
-	    cout << "================ Searching Result When We Input Company Name Provide Total Wage ===============" << endl;
-        string companyName;
-        cout << "Enter Company Name: " << endl;
-        cin >> companyName;
-
-	    for(emp_model_spc::EmpWageBuilder it : companies)
+	    for(emp_model_spc::EmpWageBuilder it : companyEmpWage.getSavedDetails())
 	    {
 		    if(it.getCompanyName() == companyName)
 		    {
